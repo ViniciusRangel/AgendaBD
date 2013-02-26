8a85a1a869c2eb191d4f33627cfaeccbf65b38c5
ViniciusRangel <vinirmv23@gmail.com>
2013-02-26 17:22:45 -0300

    [Vinicius]: Commit inicial.

---
+++ src/classes/Principal.java
@@ -0,0 +1,16 @@
+package classes;
+
+import view.JPrincipal;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		JPrincipal jp = new JPrincipal();
+		jp.setVisible(true);
+	}
+
+}
