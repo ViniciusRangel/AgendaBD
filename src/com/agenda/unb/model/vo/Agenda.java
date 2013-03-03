79782d10aab13b65aa924cf80ab62af45bfc07d8
ViniciusRangel <vinirmv23@gmail.com>
2013-03-03 13:36:42 -0300

    [Vinicius]: criação da classe modelo.vo

--- src/com/agenda/unb/model/vo/Agenda.java
+++ src/com/agenda/unb/model/vo/Agenda.java
@@ -1,9 +1,65 @@
 package com.agenda.unb.model.vo;
 
 public class Agenda {
+	
+	private int id;
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String email;
 
-	public Agenda() {
-		// TODO Auto-generated constructor stub
+	public Agenda(){
+		
+	}
+	
+	public Agenda(int id, String nome, String endereco, String telefone,String email){
+		this.id = id;
+		this.setNome(nome);
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+		
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 
 }
